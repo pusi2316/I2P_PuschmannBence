37f6cf40603b721de301c33f4c40f511808da571
pusi2316 <43464998+pusi2316@users.noreply.github.com>
2018-09-27 00:03:18 +0200

    Update Program.java

--- Program.java
+++ Program.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package linkedlist_;
+package Task2_Maven;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,41 +12,43 @@ public class Program {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        LinkedList Linked = new LinkedList();
-        Linked.Add("adsa");
-        Linked.Add("e");
-        Linked.Add("fsdfsdf");
-        Linked.Add("rwerew");
-         Linked.Add("fsdfsdfjhgjhgjg");
-        Linked.Add("rwerewfdsfdsfsd");
-        Linked.Display();
-        System.out.println("");
-        Linked.revert();
-        Linked.Display();
-        System.out.println("");
-       for (int i = 0; i < Linked.get().length; i++) {
-            System.out.print(Linked.get()[i]+" ");
-        }
-        System.out.println("");
-       Stack s=new Stack();
-        s.push("fsdfs");
-        s.push("fdvdsfsd");
-        s.push("ffsdfsdfdfs");
-        s.Display();
-        
-        System.out.println("");
-       s.revert();
-       
-       s.Display();
-        System.out.println("");
-        for (int i = 0; i < s.get().length; i++) {
-            System.out.println(s.get()[i]);
-            
-        }
-        System.out.println(s.pop());
-       
-        
-    }
-    
+	 public static void main(String[] args) {
+	        LinkedList Linked = new LinkedList();
+	        Linked.Add("adsa");
+	        Linked.Add("e");
+	        Linked.Add("fsdfsdf");
+	        Linked.Add("rwerew");
+	         Linked.Add("fsdfsdfjhgjhgjg");
+	        Linked.Add("rwerewfdsfdsfsd");
+	        Linked.Display();
+	        System.out.println("");
+	        Linked.revert();
+	        Linked.Display();
+	        System.out.println("");
+	       for (int i = 0; i < Linked.get().length; i++) {
+	            System.out.print(Linked.get()[i]+" ");
+	        }
+	        System.out.println("");
+	       Stack s=new Stack();
+	        s.push("fsdfs");
+	        s.push("fdvdsfsd");
+	        s.push("ffsdfsdfdfs");
+	        s.Display();
+	        
+	        System.out.println("");
+	       s.revert();
+	       
+	       s.Display();
+	        System.out.println("");
+	        for (int i = 0; i < s.get().length; i++) {
+	            System.out.println(s.get()[i]);
+	            
+	        }
+	        System.out.println(s.pop());
+	       
+	        
+	    }
+	    
+	}
+
 }

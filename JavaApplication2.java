4dbf6a567c905ad79975fba9bb8bdf006b8dfb93
pusi2316 <43464998+pusi2316@users.noreply.github.com>
2018-09-26 23:40:38 +0200

    Update JavaApplication2.java

--- JavaApplication2.java
+++ JavaApplication2.java
@@ -16,36 +16,52 @@ public class JavaApplication2 {
     public static String chars = "qwertzuiopasdfghjklyxcvbnmQWERTZUIOPASDFGHJKLYXCVBNM*.";
     char[] t = chars.toCharArray();
 
+    public static int shorter(String a, String b) {
+        if (a.length() < b.length()) {
+            return a.length();
+        } else if (a.length() > b.length()) {
+            return b.length();
+        } else {
+            return a.length();
+        }
+    }
 
-
-    public static boolean ReplaceWithStar(String a, String b) {
+    public static boolean ReplaceWithStar1(String a, String b) {
         boolean bool = false;
         int i = 0;
         if (a.contains("*")) {
-            while (a.charAt(i) != '*') {
+            while (a.charAt(i) != '*' && i < shorter(a, b)) {
                 if (a.charAt(i) == '.' || a.charAt(i) == b.charAt(i)) {
                     bool = true;
+                } else {
+                    bool = false;
+                    break;
                 }
                 i++;
+
             }
+
             int NumbersLeft = 0;
-            for (int k = i+1; k < a.length(); k++) {
+            for (int k = i + 1; k < a.length(); k++) {
                 NumbersLeft++;
             }
-            int k = i +1;
+
+            int k = i + 1;
+
             while (k < a.length() && k < b.length()) {
 
                 if (a.charAt(k) == '.' || (a.charAt(k) == b.charAt(b.length() - NumbersLeft))) {
                     bool = true;
                 } else {
                     bool = false;
+                    break;
                 }
                 k++;
                 NumbersLeft--;
-
             }
 
         }
+
         return bool;
     }
 
@@ -67,27 +83,49 @@ public class JavaApplication2 {
             }
             i++;
         }
+        
+        return bool;
+    }
+
+    public static boolean SpecChars(String a, String b) {
+        boolean bool = false;
+        for (int i = 0; i < a.length(); i++) {
+            if (chars.contains(a.substring(i, i+1))) {
+                bool = true;
+
+            } else {
+                bool = false;
+                break;
+            }
+        }
         return bool;
     }
 
     public static boolean StringComp(String a, String b) {
         boolean bool = false;
+        if (SpecChars(a, b) == true) {
+            if (SameCharsOrReplace(a, b)) {
 
-        if (SameCharsOrReplace(a, b)) {
+                bool = true;
 
-            bool = true;
+            } else {
+                bool = false;
+            }
+            
+            if (ReplaceWithStar1(a, b)) {
+                bool = true;
+            } 
+           if (a.length() > b.length() && a.charAt(a.length() - 1) != '*') {
+                bool = false;
+            }
         } else {
-            bool = false;
+            System.out.println("érvénytelen karakter");
         }
-        if (ReplaceWithStar(a, b)) {
-            bool = true;
-        }
-
         return bool;
     }
 
     public static void main(String[] args) {
-        boolean b = StringComp("B.dap*", "Budapest");
+        boolean b = StringComp("B*", "Budapest");
         /*System.out.println(SameChars("B.dapest", "Budapest"));
         System.out.println(Replace("B.dapest", "Budapest"));
         System.out.println(SameLength("B.dapest", "Budapest"));*/

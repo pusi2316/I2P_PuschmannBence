06e984721f5e2a4230efb06eccff13f563d9d53f
pusi2316 <43464998+pusi2316@users.noreply.github.com>
2018-09-26 23:42:31 +0200

    Update DateInput.java

--- DateInput.java
+++ DateInput.java
@@ -13,22 +13,46 @@ public class DateInput extends TextInput {
 
     int index = 0;
 
-    
-    
-    
-
     public boolean add(char c) {
+
         boolean b = true;
-        if (Character.isDigit(c) && index != 4 && index != 7) {
-            b = true;
-            s = s + c;
-            index++;
-        } else if ((index == 4 && c== '.') || (index == 7 && c == '.')) {
-            b = true;
-            s=s +c;
-            index++;
-        } else {
-            b = false;
+        if (index < 10) {
+            if (Character.isDigit(c) && index != 4 && index != 7 && index != 5 && index != 6 && index != 8 && index != 9) {
+                b = true;
+                s = s + c;
+                index++;
+
+            } else if (Character.isDigit(c) && index == 5 && (c == '0' || c == '1')) {
+                b = true;
+                s = s + c;
+                index++;
+            } else if (Character.isDigit(c) && index == 6 && (c == '0' || c == '1' || c == '2')) {
+                b = true;
+                s = s + c;
+                index++;
+            } else if (Character.isDigit(c) && index == 8 && (c == '0' || c == '1' || c == '2' || c == '3')) {
+                b = true;
+                s = s + c;
+                index++;
+            } else if (Character.isDigit(c) && index == 9) {
+                if (s.charAt(8) == '3') {
+                    if (c == '0' || c== '1'){
+                    b = true;
+                    s = s + c;
+                    index++;
+                    }
+                } else {
+                    b = true;
+                    s = s + c;
+                    index++;
+                }
+            } else if ((index == 4 && c == '.') || (index == 7 && c == '.')) {
+                b = true;
+                s = s + c;
+                index++;
+            } else {
+                b = false;
+            }
         }
 
         return b;
@@ -40,3 +64,4 @@ public class DateInput extends TextInput {
     }
 
 }
+
